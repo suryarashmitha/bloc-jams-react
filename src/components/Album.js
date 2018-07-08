d9dbc58386598822d2ae08ffa07bf1469cef49b6
RD <rd@Suryas-MacBook-Pro.local>
2018-07-08 00:05:00 -0500

    checkpoint-audioplayback:completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,51 +1,85 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
 
- class Album extends Component {
-   constructor(props) {
-     super(props);
+class Album extends Component {
+	constructor(props) {
+		super(props);
 
-     const album = albumData.find( album => {
-         return album.slug === this.props.match.params.slug
-       });
+		const album = albumData.find( album => {
+			return album.slug === this.props.match.params.slug
+		});
 
-       this.state = {
-         album: album
-       };
-     }
+		this.state = {
+			album: album,
+			currentSong: album.songs[0],
+			isPlaying: false
+		};
 
-   render() {
-     return (
-       <section className="album">
-         <section id="album-info">
-           <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
-           <div className="album-details">
-             <h1 id="album-title">{this.state.album.title}</h1>
-             <h2 className="artist">{this.state.album.artist}</h2>
-             <div id="release-info">{this.state.album.releaseInfo}</div>
-           </div>
-         </section>
-         <table id="song-list">
-          <colgroup>
-            <col id="song-number-column" />
-            <col id="song-title-column" />
-            <col id="song-duration-column" />
-          </colgroup>
-          <tbody>
-          {
-            this.state.album.songs.map( (song, index) =>
-              <tr className="album-song" key={index}>
-                <td >{index + 1}</td>
-                <td>{song.title}</td>
-                <td>{song.duration}</td>
-              </tr>
-            )
-          }
-          </tbody>
-         </table>
-       </section>
-     );
-   }
- }
+		this.audioElement = document.createElement('audio');
+		this.audioElement.src = album.songs[0].audioSrc;
+	}
 
- export default Album;
+	play() {
+		this.audioElement.play();
+		this.setState({ isPlaying: true });
+	}
+
+	pause() {
+		this.audioElement.pause();
+		this.setState({ isPlaying: false });
+	}
+
+	setSong(song) {
+		this.audioElement.src = song.audioSrc;
+		this.setState({ currentSong: song })
+	}
+
+	handleSongClick(song) {
+		const isSameSong = this.state.currentSong === song;
+		if (this.state.isPlaying && isSameSong) {
+			this.pause();
+		} else {
+			if (!isSameSong) { this.setSong(song); }
+			this.play();
+		}
+	}
+
+	render() {
+		return (
+			<section className="album">
+				<section id="album-info">
+          			<img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
+           			<div className="album-details">
+             			<h1 id="album-title">{this.state.album.title}</h1>
+             			<h2 className="artist">{this.state.album.artist}</h2>
+             		<div id="release-info">{this.state.album.releaseInfo}</div>
+           			</div>
+         		</section>
+         		<table id="song-list">
+           			<colgroup>
+             			<col id="song-number-column" />
+             			<col id="song-title-column" />
+             			<col id="song-duration-column" />
+           			</colgroup>
+
+           			<tbody>
+           			{
+					this.state.album.songs.map( (song, index) =>
+						<tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
+							<td>{index + 1}</td>
+							<td>{song.title}</td>
+							<td>{song.duration}</td>
+						</tr>
+
+					)
+					}
+           			</tbody>
+
+         		</table>
+			</section>
+		);
+	}
+}
+
+export default Album;
