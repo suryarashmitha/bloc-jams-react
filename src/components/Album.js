97b8c4d48e8bc8f28f063e24a1231491d53c2c56
RD <rd@Suryas-MacBook-Pro.local>
2018-07-08 19:33:47 -0500

    assignment-playerbarbuttons: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -72,6 +72,13 @@ import PlayerBar from './PlayerBar';
         this.setSong(newSong);
         this.play();
       }
+      handleNextClick() {
+        const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+        const newIndex = Math.min(4, currentIndex+1);
+        const newSong = this.state.album.songs[newIndex];
+        this.setSong(newSong);
+        this.play();
+      }
    render() {
      return (
        <section className="album">
@@ -106,6 +113,7 @@ import PlayerBar from './PlayerBar';
             currentSong={this.state.currentSong}
             handleSongClick={() => this.handleSongClick(this.state.currentSong)}
             handlePrevClick={() => this.handlePrevClick()}
+            handleNextClick={() => this.handleNextClick()}
           />
        </section>
      );
