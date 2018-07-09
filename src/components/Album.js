26180327bb81a96d4d3d3846b91f94fc46932225
RD <rd@Suryas-MacBook-Pro.local>
2018-07-08 19:20:11 -0500

    checkpoint-playerbarbutton: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
-
+import PlayerBar from './PlayerBar';
  class Album extends Component {
    constructor(props) {
      super(props);
@@ -65,7 +65,13 @@ import albumData from './../data/albums';
           return (index + 1)
         }
       }
-
+      handlePrevClick() {
+        const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+        const newIndex = Math.max(0, currentIndex-1);
+        const newSong = this.state.album.songs[newIndex];
+        this.setSong(newSong);
+        this.play();
+      }
    render() {
      return (
        <section className="album">
@@ -95,6 +101,12 @@ import albumData from './../data/albums';
             }
           </tbody>
          </table>
+         <PlayerBar
+            isPlaying={this.state.isPlaying}
+            currentSong={this.state.currentSong}
+            handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+            handlePrevClick={() => this.handlePrevClick()}
+          />
        </section>
      );
    }
